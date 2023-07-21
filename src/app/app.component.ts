03bf1e190dc8442098d7f78097639e1223d3cc2d
Alpha-Thibzee <thibaultbruhatpro@gmail.com>
2023-07-21 13:11:57 +0200

    cookies consent add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NgcCookieConsentService, NgcInitializationErrorEvent, NgcInitializingEvent, NgcNoCookieLawEvent, NgcStatusChangeEvent } from "ngx-cookieconsent";
+import { Subscription }   from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'transfer-flow';
+export class AppComponent implements OnInit, OnDestroy {
+
+  //keep refs to subscriptions to be able to unsubscribe later
+  private popupOpenSubscription!: Subscription;
+  private popupCloseSubscription!: Subscription;
+  private initializingSubscription!: Subscription;
+  private initializedSubscription!: Subscription;
+  private initializationErrorSubscription!: Subscription;
+  private statusChangeSubscription!: Subscription;
+  private revokeChoiceSubscription!: Subscription;
+  private noCookieLawSubscription!: Subscription;
+
+  constructor(private cookieService: NgcCookieConsentService){}
+
+  ngOnInit() {
+
+    this.popupOpenSubscription = this.cookieService.popupOpen$.subscribe(
+      () => {
+
+      });
+
+    this.popupCloseSubscription = this.cookieService.popupClose$.subscribe(
+      () => {
+
+      });
+
+    this.initializingSubscription = this.cookieService.initializing$.subscribe(
+      (event: NgcInitializingEvent) => {
+        console.log(`initializing: ${JSON.stringify(event)}`);
+      });
+
+    this.initializedSubscription = this.cookieService.initialized$.subscribe(
+      () => {
+        console.log(`initialized: ${JSON.stringify(event)}`);
+      });
+
+    this.initializationErrorSubscription = this.cookieService.initializationError$.subscribe(
+      (event: NgcInitializationErrorEvent) => {
+        console.log(`initializationError: ${JSON.stringify(event.error?.message)}`);
+      });
+
+    this.statusChangeSubscription = this.cookieService.statusChange$.subscribe(
+      (event: NgcStatusChangeEvent) => {
+
+      });
+
+    this.revokeChoiceSubscription = this.cookieService.revokeChoice$.subscribe(
+      () => {
+
+      });
+
+      this.noCookieLawSubscription = this.cookieService.noCookieLaw$.subscribe(
+      (event: NgcNoCookieLawEvent) => {
+
+      });
+  }
+
+  ngOnDestroy() {
+    this.popupOpenSubscription.unsubscribe();
+    this.popupCloseSubscription.unsubscribe();
+    this.initializingSubscription.unsubscribe();
+    this.initializedSubscription.unsubscribe();
+    this.initializationErrorSubscription.unsubscribe();
+    this.statusChangeSubscription.unsubscribe();
+    this.revokeChoiceSubscription.unsubscribe();
+    this.noCookieLawSubscription.unsubscribe();
+  }
 }
