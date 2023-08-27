d88266670667b68d8553f8730467e1f2ab799e50
Alpha-Thibzee <thibzeeweb@gmail.com>
2023-08-27 21:04:59 +0200

    change domain && google redirect

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     title: 'Prod Environment Heading',
     apiURL: 'https://api.transfer-flow.studio/api/v1/',
-    domainUrl: 'https://transfer-flow.studio',
+    domainUrl: 'transfer-flow.studio',
 };
