ca187229c557906bb1e300dc52e3dec7e5d51e4a
Alpha-Thibzee <thibzeeweb@gmail.com>
2023-09-08 09:02:43 +0200

    flash message component suite case

--- src/app/flash-message/flash-message.component.spec.ts
+++ src/app/flash-message/flash-message.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
+import { FlashMessageService } from '../../services/flash-message/flash-message.service';
 import { FlashMessageComponent } from './flash-message.component';
+
+const mockFlashMessageService = {
+  getMessage: jest.fn().mockReturnValue(of({ message: 'Test', type: 'info', duration: 1000 })),
+  deleteFlashMessage: jest.fn()
+};
 
 describe('FlashMessageComponent', () => {
   let component: FlashMessageComponent;
@@ -9,7 +15,9 @@ describe('FlashMessageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ FlashMessageComponent ],
-      imports: [ ]
+      providers: [
+        { provide: FlashMessageService, useValue: mockFlashMessageService }
+      ]
     })
     .compileComponents();
 
@@ -20,5 +28,38 @@ describe('FlashMessageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should subscribe to getMessage from FlashMessageService', () => {
+    expect(mockFlashMessageService.getMessage).toHaveBeenCalled();
+  });
+
+  it('should start animation when flash message is received', () => {
+    const spy = jest.spyOn(component, 'startAnimation');
+
+    // Trigger ngOnInit to subscribe to the observable.
+    component.ngOnInit();
+
+    // It should have started the animation.
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should delete flash message after duration', (done) => {
+    mockFlashMessageService.deleteFlashMessage.mockImplementation(() => {
+      expect(true).toBeTruthy();
+      done();
+    });
+
+    // Trigger ngOnInit to subscribe to the observable.
+    component.ngOnInit();
+
+    // Here you might need to simulate time passing and call your animation function
+  });
+
+  it('should update flashMessage when new message is received', () => {
+    component.ngOnInit();
+    expect(component.flashMessage.message).toBe('Test');
+    expect(component.flashMessage.type).toBe('info');
+    expect(component.flashMessage.duration).toBe(1000);
   });
 });
