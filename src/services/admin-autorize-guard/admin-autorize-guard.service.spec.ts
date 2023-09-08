2e3225ed54e584e55fd05d8a72bb5dd742c1ac82
Alpha-Thibzee <thibzeeweb@gmail.com>
2023-09-08 15:54:17 +0200

    admin authorize guard coverage

--- src/services/admin-autorize-guard/admin-autorize-guard.service.spec.ts
+++ src/services/admin-autorize-guard/admin-autorize-guard.service.spec.ts
@@ -1,15 +1,78 @@
 import { TestBed } from '@angular/core/testing';
-import {AdminAutorizeGuardService} from "./admin-autorize-guard.service";
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
+import { AdminAutorizeGuardService } from './admin-autorize-guard.service';
+import { CookiesService } from '../cookies/cookies.service';
+import { JwtTokenService } from '../jwt-token/jwt-token.service';
 
 describe('AdminAutorizeGuardService', () => {
   let service: AdminAutorizeGuardService;
+  let mockCookiesService: { get: jest.Mock };
+  let mockJwtTokenService: { jwtToken: string; setToken: jest.Mock; getUserRole: jest.Mock };
+  let mockRouter: { navigate: jest.Mock };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockCookiesService = {
+      get: jest.fn(),
+    };
+
+    mockJwtTokenService = {
+      jwtToken: '',
+      setToken: jest.fn(),
+      getUserRole: jest.fn(),
+    };
+
+    mockRouter = {
+      navigate: jest.fn().mockReturnValue(Promise.resolve(true)),
+    };
+
+    TestBed.configureTestingModule({
+      providers: [
+        AdminAutorizeGuardService,
+        { provide: CookiesService, useValue: mockCookiesService },
+        { provide: JwtTokenService, useValue: mockJwtTokenService },
+        { provide: Router, useValue: mockRouter },
+      ],
+    });
+
     service = TestBed.inject(AdminAutorizeGuardService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('canActivate', () => {
+    const next = {} as ActivatedRouteSnapshot;
+    const state = {} as RouterStateSnapshot;
+
+    it('should redirect to login if jwtToken is empty', async () => {
+      mockJwtTokenService.jwtToken = '';
+      const result = service.canActivate(next, state);
+      expect(mockRouter.navigate).toHaveBeenCalledWith(['/se-connecter']);
+      expect(result).toBe(false);
+    });
+
+    it('should return true if role is ADMIN', () => {
+      mockJwtTokenService.jwtToken = 'someToken';
+      mockJwtTokenService.getUserRole.mockReturnValue('ADMIN');
+      const result = service.canActivate(next, state);
+      expect(result).toBe(true);
+    });
+
+    it('should redirect to /accueil if role is USER', () => {
+      mockJwtTokenService.jwtToken = 'someToken';
+      mockJwtTokenService.getUserRole.mockReturnValue('USER');
+      const result = service.canActivate(next, state);
+      expect(mockRouter.navigate).toHaveBeenCalledWith(['/accueil']);
+      expect(result).toBe(false);
+    });
+
+    it('should redirect to login for any other roles', () => {
+      mockJwtTokenService.jwtToken = 'someToken';
+      mockJwtTokenService.getUserRole.mockReturnValue('OTHER_ROLE');
+      const result = service.canActivate(next, state);
+      expect(mockRouter.navigate).toHaveBeenCalledWith(['/se-connecter']);
+      expect(result).toBe(false);
+    });
   });
 });
--- src/services/autorize-guard/autorize-guard.service.spec.ts
+++ src/services/autorize-guard/autorize-guard.service.spec.ts
@@ -1,19 +1,81 @@
 import { TestBed } from '@angular/core/testing';
-
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { AutorizeGuardService } from './autorize-guard.service';
-import {HttpClientTestingModule} from "@angular/common/http/testing";
+import { CookiesService } from '../cookies/cookies.service';
+import { JwtTokenService } from '../jwt-token/jwt-token.service';
 
 describe('AutorizeGuardService', () => {
   let service: AutorizeGuardService;
+  let mockCookiesService: { get: jest.Mock; delete: jest.Mock };
+  let mockJwtTokenService: { jwtToken: string; setToken: jest.Mock; isTokenExpired: jest.Mock };
+  let mockRouter: { navigate: jest.Mock };
 
   beforeEach(() => {
+    mockCookiesService = {
+      get: jest.fn(),
+      delete: jest.fn(),
+    };
+
+    mockJwtTokenService = {
+      jwtToken: '',
+      setToken: jest.fn(),
+      isTokenExpired: jest.fn(),
+    };
+
+    mockRouter = {
+      navigate: jest.fn(),
+    };
+
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
+      providers: [
+        AutorizeGuardService,
+        { provide: CookiesService, useValue: mockCookiesService },
+        { provide: JwtTokenService, useValue: mockJwtTokenService },
+        { provide: Router, useValue: mockRouter },
+      ],
     });
+
     service = TestBed.inject(AutorizeGuardService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('canActivate', () => {
+    const next = {} as ActivatedRouteSnapshot;
+    const state = {} as RouterStateSnapshot;
+
+    it('should redirect to login if both jwtToken and cookie are empty', async () => {
+      mockJwtTokenService.jwtToken = '';
+      mockCookiesService.get.mockReturnValue('');
+      const result = await service.canActivate(next, state);
+      expect(mockRouter.navigate).toHaveBeenCalledWith(['/se-connecter']);
+      expect(result).toBe(false);
+    });
+
+    it('should redirect to login if token is expired', async () => {
+      mockJwtTokenService.jwtToken = 'expiredToken';
+      mockJwtTokenService.isTokenExpired.mockReturnValue(true);
+      const result = await service.canActivate(next, state);
+      expect(mockRouter.navigate).toHaveBeenCalledWith(['/se-connecter']);
+      expect(mockCookiesService.delete).toHaveBeenCalledWith('token');
+      expect(result).toBe(false);
+    });
+
+    it('should return true if token exists and is not expired', async () => {
+      mockJwtTokenService.jwtToken = 'validToken';
+      mockCookiesService.get.mockReturnValue('validToken');
+      mockJwtTokenService.isTokenExpired.mockReturnValue(false);
+      const result = await service.canActivate(next, state);
+      expect(result).toBe(true);
+    });
+
+    it('should redirect to login for any other cases', async () => {
+      // You can modify this to simulate other unexpected cases
+      const result = await service.canActivate(next, state);
+      expect(mockRouter.navigate).toHaveBeenCalledWith(['/se-connecter']);
+      expect(result).toBe(false);
+    });
   });
 });
