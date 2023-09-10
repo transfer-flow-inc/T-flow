8a0e5dc7cfb6b22d01da7727628354dc16bd9e56
Alpha-Thibzee <thibzeeweb@gmail.com>
2023-09-10 21:11:41 +0200

    app component test coverage

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,37 +1,92 @@
-import {TestBed} from '@angular/core/testing';
-import {RouterTestingModule} from '@angular/router/testing';
-import {AppComponent} from './app.component';
-import {NavbarComponent} from "./navbar/navbar.component";
-import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
+import {ComponentFixture,TestBed} from '@angular/core/testing';
+import { DOCUMENT } from '@angular/common';
+import { AppComponent } from './app.component';
+import { LocalStorageService } from '../services/local-storage/local-storage.service';
+import {DateTimeProvider, OAuthLogger, OAuthService, UrlHelperService} from "angular-oauth2-oidc";
+import {RouterOutlet} from "@angular/router";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
-import {OAuthModule} from "angular-oauth2-oidc";
+import {NavbarComponent} from "./navbar/navbar.component";
 import {FooterComponent} from "./footer/footer.component";
+import {CookiesConsentComponent} from "./cookies-consent/cookies-consent.component";
+import {Title} from "@angular/platform-browser";
+import {FlashMessageComponent} from "./flash-message/flash-message.component";
+import {NgcCookieConsentConfig, NgcCookieConsentService, WindowService} from "ngx-cookieconsent";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {RouterTestingModule} from "@angular/router/testing";
+
 
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+  let localStorageService: LocalStorageService;
+
+  beforeEach(() => {
 
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        HttpClientTestingModule,
-        OAuthModule.forRoot()
-      ],
-      declarations: [
-        AppComponent,
+    TestBed.configureTestingModule({
+      declarations: [AppComponent,
         NavbarComponent,
+        FooterComponent,
+        CookiesConsentComponent,
+        FlashMessageComponent],
+      imports: [ HttpClientTestingModule, FontAwesomeModule, RouterTestingModule.withRoutes([])],
+      providers: [
+        { provide: DOCUMENT, useValue: document },
+        NavbarComponent,
+        FooterComponent,
+        CookiesConsentComponent,
+        LocalStorageService,
+        OAuthService,
+        OAuthLogger,
+        UrlHelperService,
+        DateTimeProvider,
+        RouterOutlet,
+        Title,
+        NgcCookieConsentService,
+        WindowService,
+        NgcCookieConsentConfig,
       ],
-      providers: [NavbarComponent, FooterComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    }).compileComponents();
+    });
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    localStorageService = TestBed.inject(LocalStorageService);
   });
 
-  test('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('should handle the theme correctly', () => {
+    const getSpy = spyOn(localStorageService, 'get').and.returnValue('dark');
+
+    component.handleTheme();
+
+    expect(document.body.classList).toContain('dark');
+
+    expect(getSpy).toHaveBeenCalledWith('theme');
   });
 
+  it('should log console warnings', () => {
+  // Create spies for console.log to capture log messages
+  const consoleLogSpy = spyOn(console, 'log');
+
+  // Call the logConsoleWarnings method
+  component.logConsoleWarnings();
+
+  // Expect console.log to have been called with the expected warning messages
+  expect(consoleLogSpy.calls.allArgs()).toEqual([
+    ['%cHold Up!', 'color:red; font-size: 6rem; font-weight: bold;'],
+    [
+      "%cIf someone told you to copy and paste something here, they're trying to hack you. Don't do it!",
+      "color: white; font-size: 20px; font-weight: bold;"
+    ],
+    ['%cAttention!', 'color:red; font-size: 6rem; font-weight: bold;'],
+    [
+      "%cSi quelqu'un vous a dit de copier et coller quelque chose ici, il essaie de vous pirater. Ne le faites pas!",
+      "color: white; font-size: 20px; font-weight: bold;"
+    ],
+  ]);
 });
 
+
+
+
+});
--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -1,78 +1,150 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavbarComponent } from './navbar.component';
-import { FontAwesomeTestingModule } from "@fortawesome/angular-fontawesome/testing";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
-import { OAuthModule } from "angular-oauth2-oidc";
-import { FooterComponent } from "../footer/footer.component";
-import { ThemeServiceService } from "../../services/theme-service/theme-service.service";
-import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
-import { BehaviorSubject, of } from "rxjs";
-import { HttpClientService } from "../../services/httpClient/http-client.service";
+import { ThemeServiceService } from '../../services/theme-service/theme-service.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {HttpClient} from "@angular/common/http";
+import {DateTimeProvider, OAuthLogger, OAuthService, UrlHelperService} from "angular-oauth2-oidc";
+import {FooterComponent} from "../footer/footer.component";
+import {HttpClientService} from "../../services/httpClient/http-client.service";
+import {JwtTokenService} from "../../services/jwt-token/jwt-token.service";
+import {CookiesService} from "../../services/cookies/cookies.service";
+import {of} from "rxjs";
 
-const themeServiceMock = {
-  toggleTheme: jest.fn(),
-  currentTheme$: of('light')
-};
-
-const httpClientServiceMock = {
-  isAuthenticated$: new BehaviorSubject(false),
-};
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
+ let mockHttpClientService: Partial<HttpClientService>;
+  let mockJwtTokenService: Partial<JwtTokenService>;
+  let mockCookiesService: Partial<CookiesService>;
+let mockThemeService: Partial<ThemeServiceService>;
+  let mockFooterComponent: Partial<FooterComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(() => {
+
+    mockHttpClientService = {
+      isAuthenticated$: of(true), // Mock the observable with a value
+    };
+
+    mockJwtTokenService = {
+      getUserRole: () => 'ADMIN', // Mock the getUserRole method
+    };
+
+    mockCookiesService = {
+      get: () => 'mock-token', // Mock the get method
+    };
+
+    mockThemeService = {
+      getCurrentTheme: () => 'light', // Mock the getCurrentTheme method
+      toggleTheme: () => {}, // Mock the toggleTheme method
+    };
+
+    mockFooterComponent = {
+      ngOnInit: () => {}, // Mock the ngOnInit method of FooterComponent
+    };
+
+
+
+    TestBed.configureTestingModule({
       declarations: [NavbarComponent],
-      imports: [FontAwesomeTestingModule, HttpClientTestingModule, OAuthModule.forRoot()],
+      imports: [FontAwesomeModule, HttpClientTestingModule],
       providers: [
-        { provide: ThemeServiceService, useValue: themeServiceMock },
-        { provide: HttpClientService, useValue: httpClientServiceMock },
+        { provide: HttpClientService, useValue: mockHttpClientService },
+        { provide: JwtTokenService, useValue: mockJwtTokenService },
+        { provide: CookiesService, useValue: mockCookiesService },
+        ThemeServiceService,
+        HttpClient,
+        OAuthService,
+        DateTimeProvider,
+        UrlHelperService,
+        OAuthLogger,
         FooterComponent
-      ]
-    }).compileComponents();
+      ],
+    });
 
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    spyOn(FooterComponent.prototype, 'ngOnInit').and.callThrough();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should toggle menu correctly', () => {
-    expect(component.isMenuOpen).toBeFalsy();
-    component.openMenu();
-    expect(component.isMenuOpen).toBeTruthy();
-    expect(component.navIcon).toEqual(faXmark);
-    component.openMenu();
-    expect(component.isMenuOpen).toBeFalsy();
-    expect(component.navIcon).toEqual(faBars);
+  it('should toggle isPopupOpen and update popupToggleValue', () => {
+  // Initial state: isPopupOpen should be false, and popupToggleValue should be 'hidden'
+  expect(component.isPopupOpen).toBe(false);
+  expect(component.popupToggleValue).toBe('hidden');
+
+  // Call togglePopup() to open the popup
+  component.togglePopup();
+
+  // After calling togglePopup, isPopupOpen should be true, and popupToggleValue should be 'show'
+  expect(component.isPopupOpen).toBe(true);
+  expect(component.popupToggleValue).toBe('show');
+
+  // Call togglePopup() again to close the popup
+  component.togglePopup();
+
+  // After the second call, isPopupOpen should be false again, and popupToggleValue should be 'reversePopup'
+  expect(component.isPopupOpen).toBe(false);
+  expect(component.popupToggleValue).toBe('reversePopup');
+});
+
+  it('should toggle isMenuOpen and update navbarToggleValue and iconShow', () => {
+  // Initial state: isMenuOpen should be false, navbarToggleValue should be 'hide', and iconShow should be 'show'
+  expect(component.isMenuOpen).toBe(false);
+  expect(component.navbarToggleValue).toBe('hide');
+  expect(component.iconShow).toBe('show');
+
+  // Call openMenu() to open the menu
+  component.openMenu();
+
+  // After calling openMenu, isMenuOpen should be true, navbarToggleValue should be 'showNav', and iconShow should be 'hidden'
+  expect(component.isMenuOpen).toBe(true);
+  expect(component.navbarToggleValue).toBe('showNav');
+  expect(component.iconShow).toBe('hidden');
+
+  // Call openMenu() again to close the menu
+  component.openMenu();
+
+  // After the second call, isMenuOpen should be false again, navbarToggleValue should be 'reverse', and iconShow should be 'hidden'
+  expect(component.isMenuOpen).toBe(false);
+  expect(component.navbarToggleValue).toBe('reverse');
+  expect(component.iconShow).toBe('hidden');
+});
+
+  it('should initialize isAuthenticated based on HttpClientService', () => {
+    expect(component.isAuthenticated).toBe(false);
+
+    fixture.detectChanges(); // Trigger ngOnInit
+
+    expect(component.isAuthenticated).toBe(true);
   });
 
-  it('should toggle popup correctly', () => {
-    expect(component.isPopupOpen).toBeFalsy();
-    component.togglePopup();
-    expect(component.isPopupOpen).toBeTruthy();
-    component.togglePopup();
-    expect(component.isPopupOpen).toBeFalsy();
+  it('should initialize isAdministrator based on JwtTokenService and CookiesService', () => {
+    expect(component.isAdministrator).toBe(false);
+
+    fixture.detectChanges(); // Trigger ngOnInit
+
+    expect(component.isAdministrator).toBe(true);
   });
 
-  it('should toggle theme', () => {
-    component.toggleTheme();
-    expect(themeServiceMock.toggleTheme).toHaveBeenCalled();
-  });
+  it('should initialize isDarkTheme and imgTheme based on localStorage', () => {
+  // Set the theme to 'light' in localStorage
+  localStorage.setItem('theme', 'light');
 
-  it('should change isAuthenticated when HttpClientService emits', () => {
-    expect(component.isAuthenticated).toBeFalsy();
-    httpClientServiceMock.isAuthenticated$.next(true);
-    expect(component.isAuthenticated).toBeTruthy();
-  });
+  // Expect isDarkTheme to be true because 'light' is not 'light'
+  expect(component.isDarkTheme).toBe(true);
 
-  it('should initialize correctly', () => {
-    component.ngOnInit();
-    expect(component.imgTheme).toEqual('assets/images/logo_with_text_dark.png'); // Or whatever you expect
-  });
+  // Expect imgTheme to be 'assets/images/logo_with_text_dark.png'
+  expect(component.imgTheme).toBe('assets/images/logo_with_text_dark.png');
+});
+
+
+
+
+
+
 });
