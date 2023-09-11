aa75f05da93042d99f2470bba2a890d3344a53f2
Alpha-Thibzee <thibzeeweb@gmail.com>
2023-09-11 18:25:28 +0200

    more more more coverage

--- src/services/http-interceptor/http-interceptor.service.ts
+++ src/services/http-interceptor/http-interceptor.service.ts
@@ -1,7 +1,8 @@
-import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {CookiesService} from "../cookies/cookies.service";
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable} from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { CookiesService } from '../cookies/cookies.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,23 +11,26 @@ export class HttpInterceptorService implements HttpInterceptor {
 
   constructor(
     private cookiesService: CookiesService,
-  ) {
-  }
+  ) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let modifiedReq = req;
 
     const token = this.cookiesService.get('token');
     if (token) {
-      const cloned = req.clone({
-        headers: req.headers.set("Authorization",
-          "Bearer " + token)
+      modifiedReq = req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${token}`)
       });
-
-      return next.handle(cloned);
     }
 
-    return next.handle(req);
-
-
+    return next.handle(modifiedReq).pipe(
+      catchError((error) => {
+        if (error.status === 401) {
+          this.cookiesService.delete('token');
+          sessionStorage.clear();
+        }
+         throw error;
+      })
+    )
   }
 }
