ac018986bafa0e2d42b101c83d6cec4ca3cd6bd2
Alpha-Thibzee <thibzeeweb@gmail.com>
2023-09-12 18:34:07 +0200

    all transfer coverage

--- src/app/settings-all-transfer/settings-all-transfer.component.spec.ts
+++ src/app/settings-all-transfer/settings-all-transfer.component.spec.ts
@@ -1,29 +1,62 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SettingsAllTransferComponent } from './settings-all-transfer.component';
+import { JwtTokenService } from '../../services/jwt-token/jwt-token.service';
+import { HttpClientService } from '../../services/httpClient/http-client.service';
+import { of, throwError } from 'rxjs';
 import {SettingsNavbarComponent} from "../settings-navbar/settings-navbar.component";
-import {HttpClientTestingModule} from "@angular/common/http/testing";
-import {DateTimeProvider, OAuthLogger, OAuthService, UrlHelperService} from "angular-oauth2-oidc";
 
 describe('SettingsAllTransferComponent', () => {
   let component: SettingsAllTransferComponent;
   let fixture: ComponentFixture<SettingsAllTransferComponent>;
+  let mockJwtTokenService: Partial<JwtTokenService>;
+  let mockHttpClientService: Partial<HttpClientService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SettingsAllTransferComponent, SettingsNavbarComponent ],
+  beforeEach(() => {
+    mockJwtTokenService = {
+      getToken: jest.fn().mockReturnValue('some-token'),
+      getUserId: jest.fn().mockReturnValue('user-id'),
+    };
+
+    mockHttpClientService = {
+      getAllFolderByUserId: jest.fn().mockReturnValue(of([{ id: '1', name: 'folder1' }]))
+    };
+
+    TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [OAuthService, UrlHelperService, OAuthLogger, DateTimeProvider]
-    })
-    .compileComponents();
+      declarations: [SettingsAllTransferComponent, SettingsNavbarComponent],
+      providers: [
+        { provide: JwtTokenService, useValue: mockJwtTokenService },
+        { provide: HttpClientService, useValue: mockHttpClientService },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SettingsAllTransferComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should initialize user ID from JwtTokenService', () => {
+    component.ngOnInit();
+    expect(mockJwtTokenService.getUserId).toHaveBeenCalled();
+    expect(component.userId).toBe('user-id');
+  });
+
+  it('should load all folders when initialized', () => {
+    component.ngOnInit();
+    expect(mockHttpClientService.getAllFolderByUserId).toHaveBeenCalled();
+    expect(component.allFolder.length).toBe(1);
+    expect(component.isFolderEmpty).toBe(false);
+  });
+
+  it('should set error message when API call fails', () => {
+    mockHttpClientService.getAllFolderByUserId = jest.fn().mockReturnValue(throwError('An error occurred'));
+    component.ngOnInit();
+    expect(component.errorMessage).toBe(true);
+  });
+
+  // ... more tests as needed
 });
