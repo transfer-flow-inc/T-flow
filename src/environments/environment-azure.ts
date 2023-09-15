2d0c87d37ee51aacb87092737ed4a406b09a442a
Thibault BRUHAT <thibaultbruhatpro@gmail.com>
2023-09-15 10:35:07 +0200

    add an environment to work with azure

--- src/environments/environment-azure.ts
+++ src/environments/environment-azure.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   isAzure: true,
   title: 'Prod Environment Heading',
-  apiURL: 'https://api.transfer-flow.studio/api/v1/',
+  apiURL: 'https://azure.api.transfer-flow.studio/api/v1/',
   domainUrl: 'azure.transfer-flow.studio',
   protocol: 'https://',
   port: ''
