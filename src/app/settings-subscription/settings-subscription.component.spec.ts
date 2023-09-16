b7e07bb2143d5a5eb101ed3d6247f20613c33430
Alpha-Thibzee <thibzeeweb@gmail.com>
2023-09-16 17:23:55 +0200

    coverage on login component

--- src/app/settings-subscription/settings-subscription.component.spec.ts
+++ src/app/settings-subscription/settings-subscription.component.spec.ts
@@ -2,14 +2,33 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SettingsSubscriptionComponent } from './settings-subscription.component';
 import {SettingsNavbarComponent} from "../settings-navbar/settings-navbar.component";
+import {CookiesService} from "../../services/cookies/cookies.service";
+import {JwtTokenService} from "../../services/jwt-token/jwt-token.service";
 
 describe('SettingsSubscriptionComponent', () => {
   let component: SettingsSubscriptionComponent;
   let fixture: ComponentFixture<SettingsSubscriptionComponent>;
+  let mockCookiesService : any;
+  let mockJwtTokenService : any;
+  let mockRouter: any;
 
   beforeEach(async () => {
+     mockCookiesService = {
+      get: jest.fn()
+    };
+    mockJwtTokenService = {
+      getUserPlan: jest.fn(),
+      setToken: jest.fn()
+    }
+    mockRouter = {
+      navigate: jest.fn()
+    }
     await TestBed.configureTestingModule({
-      declarations: [ SettingsSubscriptionComponent, SettingsNavbarComponent ]
+      declarations: [ SettingsSubscriptionComponent, SettingsNavbarComponent ],
+      providers: [
+        { provide: CookiesService, useValue: mockCookiesService },
+        { provide: JwtTokenService, useValue: mockJwtTokenService }
+      ]
     })
     .compileComponents();
 
@@ -21,4 +40,22 @@ describe('SettingsSubscriptionComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call get Token from cookies', () => {
+    component.getTokenFromCookies();
+  });
+
+
+  it('should call set user plan', () => {
+    component.setUserPlan('token');
+  });
+
+  it('should return appropriate plan text', () => {
+    expect(component.getReadablePlan('FREE')).toBe("Vous n'avez pas d'abonnement actif");
+    expect(component.getReadablePlan('PREMIUM')).toBe('Premium');
+    expect(component.getReadablePlan('ULTIMATE')).toBe('Ultimate');
+    expect(component.getReadablePlan('UNKNOWN')).toBe('Unspecified');
+  });
+
+
 });
