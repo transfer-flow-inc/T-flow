882095bfc4f1e6b63f5a8d99da83dab64dd59ce7
Alpha-Thibzee <thibzeeweb@gmail.com>
2023-09-17 12:58:37 +0200

    my account more coverage

--- src/app/settings-my-account/settings-my-account.component.spec.ts
+++ src/app/settings-my-account/settings-my-account.component.spec.ts
@@ -7,12 +7,16 @@ import {DateTimeProvider, OAuthLogger, OAuthService, UrlHelperService} from "ang
 import {JwtTokenService} from "../../services/jwt-token/jwt-token.service";
 import {CookiesService} from "../../services/cookies/cookies.service";
 import {Router} from "@angular/router";
+import {HttpClientService} from "../../services/httpClient/http-client.service";
+import {of, throwError} from "rxjs";
+import {environment} from "../../environments/environment";
 describe('SettingsMyAccountComponent', () => {
   let component: SettingsMyAccountComponent;
   let fixture: ComponentFixture<SettingsMyAccountComponent>;
   let jwtService: JwtTokenService;
   let cookiesService: CookiesService;
   let router: Router;
+  let httpClient: HttpClientService;
 
   beforeEach( () => {
 
@@ -25,17 +29,23 @@ describe('SettingsMyAccountComponent', () => {
     const mockRouter = {
       navigate: jest.fn()
     }
+    const mockHttpClient = {
+      updateUser: jest.fn()
+    }
     TestBed.configureTestingModule({
       declarations: [SettingsMyAccountComponent, SettingsNavbarComponent],
       imports: [RouterTestingModule],
       providers: [HttpClient, HttpHandler, OAuthService, UrlHelperService, OAuthLogger, DateTimeProvider,
       { provide: JwtTokenService, useValue: mockJwtService },
-      { provide: CookiesService, useValue: mockCookiesService }
+      { provide: CookiesService, useValue: mockCookiesService },
+        {provide: HttpClientService, useValue: mockHttpClient},
       ],
     }).compileComponents();
 
     jwtService = TestBed.inject(JwtTokenService);
     cookiesService = TestBed.inject(CookiesService);
+    httpClient = TestBed.inject(HttpClientService);
+
     fixture = TestBed.createComponent(SettingsMyAccountComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -85,6 +95,39 @@ describe('SettingsMyAccountComponent', () => {
     component.toggleUpdateUser(); // call the method again
     expect(component.isUpdateUser).toBe(false); // verify that it was toggled back to false
   });
+
+it('should submit user updates correctly', () => {
+    const userUpdate = {
+      lastName: 'Doe',
+      firstName: 'John',
+      email: 'test@test.fr',
+      oldPassword: 'oldPassword',
+      password: 'password'
+    };
+    const updateUserUrl = 'http://updateUserUrl.com';
+    const mockResponse = { token: 'Update successful' };
+
+    component.userUpdate = userUpdate;
+
+    // Mock the component methods and httpClient method
+    jest.spyOn(component, 'createUserUpdateObject').mockReturnValue(userUpdate);
+    jest.spyOn(component, 'buildUpdateUserUrl').mockReturnValue(updateUserUrl);
+    jest.spyOn(component, 'handleUpdateSuccess');
+    jest.spyOn(component, 'handleUpdateError');
+    jest.spyOn(httpClient, 'updateUser').mockReturnValue(of(mockResponse));
+
+    component.submitUpdateUser();
+
+    expect(component.createUserUpdateObject).toHaveBeenCalled();
+    expect(component.buildUpdateUserUrl).toHaveBeenCalled();
+    expect(httpClient.updateUser).toHaveBeenCalledWith(updateUserUrl, userUpdate);
+    expect(component.handleUpdateSuccess).toHaveBeenCalledWith(mockResponse);
+    expect(component.handleUpdateError).not.toHaveBeenCalled();
+  });
+
+
+
+
 
 
 
