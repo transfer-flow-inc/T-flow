f19805ec265031f26f6f52d665e7a53c47ed96d8
Alpha-Thibzee <thibzeeweb@gmail.com>
2023-09-17 20:55:33 +0200

    add coverage to providers

--- src/app/dashboard-all-providers/dashboard-all-providers.component.spec.ts
+++ src/app/dashboard-all-providers/dashboard-all-providers.component.spec.ts
@@ -4,10 +4,12 @@ import { DashboardAllProvidersComponent } from './dashboard-all-providers.compon
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {FontAwesomeTestingModule} from "@fortawesome/angular-fontawesome/testing";
 import {DashboardNavbarComponent} from "../dashboard-navbar/dashboard-navbar.component";
+import {ThemeServiceService} from "../../services/theme-service/theme-service.service";
 
 describe('DashboardAllProvidersComponent', () => {
   let component: DashboardAllProvidersComponent;
   let fixture: ComponentFixture<DashboardAllProvidersComponent>;
+  let themeService: ThemeServiceService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,6 +19,7 @@ describe('DashboardAllProvidersComponent', () => {
     .compileComponents();
 
     fixture = TestBed.createComponent(DashboardAllProvidersComponent);
+    themeService = TestBed.inject(ThemeServiceService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -24,4 +27,15 @@ describe('DashboardAllProvidersComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+   it('should update logos based on theme', () => {
+   themeService.currentThemeSubject.next('light');
+    component.ngOnInit();
+
+    expect(component.logoGithub).toBe('assets/images/logo_github_dark.png');
+    expect(component.logoSonarQube).toBe('assets/images/logo_sonarqube.svg');
+  });
+
+
+
 });
