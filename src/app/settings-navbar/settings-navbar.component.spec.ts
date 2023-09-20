c249a9fa25855a052d06a6fa64fd614f6970a320
Thibault BRUHAT <thibaultbruhatpro@gmail.com>
2023-09-20 11:06:13 +0200

    refactor is folder expired test

--- src/app/settings-navbar/settings-navbar.component.spec.ts
+++ src/app/settings-navbar/settings-navbar.component.spec.ts
@@ -1,10 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SettingsNavbarComponent } from './settings-navbar.component';
+import {JwtTokenService} from "../../services/jwt-token/jwt-token.service";
+import {CookiesService} from "../../services/cookies/cookies.service";
 
 describe('SettingsNavbarComponent', () => {
   let component: SettingsNavbarComponent;
   let fixture: ComponentFixture<SettingsNavbarComponent>;
+  let cookiesService: CookiesService;
+  let jwtService: JwtTokenService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -12,6 +16,8 @@ describe('SettingsNavbarComponent', () => {
     })
     .compileComponents();
 
+    cookiesService = TestBed.inject(CookiesService);
+    jwtService = TestBed.inject(JwtTokenService);
     fixture = TestBed.createComponent(SettingsNavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +25,11 @@ describe('SettingsNavbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get the user token from cookies', () => {
+    cookiesService.set('token', 'test', 30);
+    expect(cookiesService.get('token')).toEqual('test');
+    component.ngOnInit();
   });
 });
